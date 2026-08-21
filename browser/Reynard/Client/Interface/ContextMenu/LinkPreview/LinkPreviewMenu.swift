a7f7decef4e3d8c94621a8915be1eb0c210c1777
㗽翳 <115837765+xytxg@users.noreply.github.com>
2026-08-21 21:09:06 +0800

    sync upstream Firefox 154: browser/Reynard/Client/Interface/ContextMenu/LinkPreview/LinkPreviewMenu.swift

--- browser/Reynard/Client/Interface/ContextMenu/LinkPreview/LinkPreviewMenu.swift
+++ browser/Reynard/Client/Interface/ContextMenu/LinkPreview/LinkPreviewMenu.swift
@@ -13,11 +13,12 @@ struct LinkPreviewMenu {
         showsPreview: Bool,
         isPrivate: Bool,
         sessionManager: SessionManager,
+        sourceView: UIView,
         onPreviewCreated: @escaping (LinkPreviewViewController) -> Void,
         openInNewTab: @escaping () -> Void,
         openInNewPrivateTab: @escaping () -> Void,
         openInBackground: @escaping () -> Void,
-        shareLink: @escaping (URL) -> Void
+        shareLink: @escaping (URL, UIView, CGRect) -> Void
     ) -> UIContextMenuConfiguration? {
         guard case .link(let url) = context.target else {
             return nil
@@ -35,21 +36,29 @@ struct LinkPreviewMenu {
         
         return UIContextMenuConfiguration(identifier: url as NSURL, previewProvider: previewProvider) { _ in
             UIMenu(title: "", children: [
-                UIAction(title: NSLocalizedString("Open in New Tab", comment: ""), image: UIImage(named: "reynard.plus.square")) { _ in
-                    openInNewTab()
-                },
-                UIAction(title: NSLocalizedString("Open in New Private Tab", comment: ""), image: UIImage(named: "reynard.plus.square.fill")) { _ in
-                    openInNewPrivateTab()
-                },
-                UIAction(title: NSLocalizedString("Open in Background", comment: ""), image: UIImage(named: "reynard.plus.square.dashed")) { _ in
-                    openInBackground()
-                },
-                UIAction(title: NSLocalizedString("Copy Link", comment: ""), image: UIImage(named: "reynard.document.on.document")) { _ in
-                    UIPasteboard.general.string = url.absoluteString
-                },
-                UIAction(title: NSLocalizedString("Share Link", comment: ""), image: UIImage(named: "reynard.square.and.arrow.up")) { _ in
-                    shareLink(url)
-                },
+                UIMenu(title: "", options: .displayInline, children: [
+                    UIAction(title: NSLocalizedString("Open Link in New Tab", comment: ""), image: UIImage(named: "reynard.plus.square")) { _ in
+                        openInNewTab()
+                    },
+                    UIAction(title: NSLocalizedString("Open Link in New Private Tab", comment: ""), image: UIImage(named: "reynard.plus.square.fill")) { _ in
+                        openInNewPrivateTab()
+                    },
+                    UIAction(title: NSLocalizedString("Open Link in Background", comment: ""), image: UIImage(named: "reynard.plus.square.dashed")) { _ in
+                        openInBackground()
+                    },
+                ]),
+                UIMenu(title: "", options: .displayInline, children: [
+                    UIAction(title: NSLocalizedString("Copy Link", comment: ""), image: UIImage(named: "reynard.document.on.document")) { _ in
+                        UIPasteboard.general.string = url.absoluteString
+                    },
+                    UIAction(title: NSLocalizedString("Share Link", comment: ""), image: UIImage(named: "reynard.square.and.arrow.up")) { _ in
+                        shareLink(
+                            url,
+                            sourceView,
+                            CGRect(origin: context.point, size: .zero)
+                        )
+                    },
+                ]),
             ])
         }
     }
